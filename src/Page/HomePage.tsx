27a95d3ec06b3ae7b9328a857a83aa063606f415
ImMasterSam <sam9630147@gmail.com>
2025-06-22 22:41:23 +0800

    Add few content to home page

--- src/Page/HomePage.tsx
+++ src/Page/HomePage.tsx
@@ -1,11 +1,20 @@
-import './Page.css'
+import "./Page.css";
 
 function HomePage() {
   return (
-    <>
-      <h2>Welcom to F1 Dash</h2>
-    </>
-  )
+    <div className="home">
+      <h2 style={{padding: '15px 10px'}}>Welcom to F1 Dash</h2>
+      <p style={{padding: '10px'}}>
+        This project uses the
+        {" "}
+        <span>
+          <a href="https://api.jolpi.ca/ergast/" style={{color: 'white'}}>jolpi.ca F1 API</a>
+        </span>{" "}
+        to fetch Formula 1 data.
+      </p>
+      <a href="https://github.com/ImMasterSam/F1-Data" style={{padding: '10px', color: 'white'}}>Source Code</a>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
