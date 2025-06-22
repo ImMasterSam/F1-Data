f59558e703f9fd0bf80b2768e5873a88b622a66b
ImMasterSam <sam9630147@gmail.com>
2025-06-22 17:08:12 +0800

    Add table title

--- src/Component/Standing/table/ConstructorStandingTable.tsx
+++ src/Component/Standing/table/ConstructorStandingTable.tsx
@@ -7,28 +7,32 @@ type Props = {
 
 function ConstructorStandingTable({constructorStanding}: Props) {
   return (
-    <table className="constructorStanding-table">
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>Constructor</th>
-            <th>Win(s)</th>
-            <th>Points</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {constructorStanding.map((constructor) => {
-            return <tr key={constructor.position}>
-              <td>{constructor.position}</td>
-              <td>{constructor.Constructor.name}</td>
-              <td>{constructor.wins}</td>
-              <td>{constructor.points}</td>
+    <div className="standings">
+      <h2 className="table-title">Constructor Championship Standings</h2>
+      <table className="constructorStanding-table">
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Constructor</th>
+              <th>Win(s)</th>
+              <th>Points</th>
             </tr>
-          })}   
-        </tbody>
+          </thead>
 
-    </table>
+          <tbody>
+            {constructorStanding.map((constructor) => {
+              return <tr key={constructor.position}>
+                <td>{constructor.position}</td>
+                <td>{constructor.Constructor.name}</td>
+                <td>{constructor.wins}</td>
+                <td>{constructor.points}</td>
+              </tr>
+            })}   
+          </tbody>
+
+      </table>
+    </div>
+
   );
 }
 
--- src/Component/Standing/table/DriverStandingTable.tsx
+++ src/Component/Standing/table/DriverStandingTable.tsx
@@ -7,28 +7,32 @@ type Props = {
 
 function DriverStandingTable({driverStanding: constructorStanding}: Props) {
   return (
-    <table className="driverStanding-table">
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>Driver</th>
-            <th>Win(s)</th>
-            <th>Points</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {constructorStanding.map((constructor) => {
-            return <tr key={constructor.position}>
-              <td>{constructor.position}</td>
-              <td>{constructor.Driver.familyName}</td>
-              <td>{constructor.wins}</td>
-              <td>{constructor.points}</td>
+    <div className="standings">
+      <h2 className="table-title">Driver Championship Standings</h2>
+      <table className="driverStanding-table">
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Driver</th>
+              <th>Win(s)</th>
+              <th>Points</th>
             </tr>
-          })}   
-        </tbody>
+          </thead>
 
-    </table>
+          <tbody>
+            {constructorStanding.map((constructor) => {
+              return <tr key={constructor.position}>
+                <td>{constructor.position}</td>
+                <td>{constructor.Driver.familyName}</td>
+                <td>{constructor.wins}</td>
+                <td>{constructor.points}</td>
+              </tr>
+            })}   
+          </tbody>
+
+      </table>
+    </div>
+
   );
 }
 
