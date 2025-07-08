853b0088b778336556155e899655bdb140a10a52
ImMasterSam <sam9630147@gmail.com>
2025-07-08 20:05:24 +0800

    Change side bar icon

--- src/Component/SideNav/SideNav.tsx
+++ src/Component/SideNav/SideNav.tsx
@@ -6,6 +6,7 @@ import { AiFillHome } from "react-icons/ai";
 import { BsBroadcast } from "react-icons/bs";
 import { FaTrophy } from "react-icons/fa";
 import { RiCalendarScheduleFill } from "react-icons/ri";
+import { LuPanelLeftOpen, LuPanelRightOpen } from "react-icons/lu";
 
 const NavItems = [
   { label: 'Home', href: '/', icon: <AiFillHome /> },
@@ -23,7 +24,9 @@ function SideNav() {
   }
 
   return <nav className='side-bar'>
-    <button onClick={toggleExpanded}>{isExpanded ? 'Close' : 'Open'}</button>
+    <button onClick={toggleExpanded}>
+      {isExpanded ? <LuPanelRightOpen /> : <LuPanelLeftOpen />}
+    </button>
     {NavItems.map((nav, index) => {
       return <NavItem navItem={nav} index={index} isExpanded={isExpanded}/>
     })}
