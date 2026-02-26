7b92f33cd3bcfb5210bf88a191e67fdc4f00a0fd
ImMasterSam <sam9630147@gmail.com>
2026-02-27 00:09:41 +0800

    Add round number to schedule block

--- src/Component/Schedule/ScheduleBlock.tsx
+++ src/Component/Schedule/ScheduleBlock.tsx
@@ -10,15 +10,21 @@ type Props = {
 
 function ScheduleBlock({race, setSelectedTrack}: Props) {
   return <div className={`race-block ${race.status}`} onClick={() => {setSelectedTrack(race.round-1)}}>
-    <div className="race-info">
-      <div className="race-header">
-        <ReactCountryFlag countryCode={Country[race.Circuit.Location.country]} 
-        aria-label={race.Circuit.Location.country}
-        style={{scale: 1.5}} svg />
-        <h3>{race.raceName}</h3>
+    <div className="race-block-left">
+      <div className="race-round-number">
+        <h2>{race.round}</h2>
       </div>
-      <p>{race.Circuit.circuitName}</p>
+      <div className="race-info">
+        <div className="race-header">
+          <ReactCountryFlag countryCode={Country[race.Circuit.Location.country]} 
+          aria-label={race.Circuit.Location.country}
+          style={{scale: 1.5}} svg />
+          <h3>{race.raceName}</h3>
+        </div>
+        <p>{race.Circuit.circuitName}</p>
+      </div>
     </div>
+
     <div className="race-date">
       <p>{getDateRange(race.date)}</p>
     </div>
