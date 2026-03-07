2b68c46b1dcc531030beeb4ab0783ac9bb5da885
Master Sam <105840191+ImMasterSam@users.noreply.github.com>
2026-03-07 23:53:03 +0800

    Revert "Fix: Revert standings page back to 2025"

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://F1-Data/config/
+// https://vite.dev/config/
 export default defineConfig({
   base: '/F1-Data',
   plugins: [react()],
