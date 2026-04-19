0cff8e2800fb787e325a394dd010fe08d0ec800e
ImMasterSam <sam9630147@gmail.com>
2026-04-20 00:38:33 +0800

    New home page

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         <div className='main-container'>
 
             <div className='main-title'>
-              <h1>F1 Data Analysis</h1>
+              <h1>F1 Laboratory</h1>
               <a href='https://github.com/ImMasterSam/F1-Data' target="_blank" rel="noopener noreferrer">
                 <FaGithub />
               </a>
